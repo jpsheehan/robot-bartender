686c3576ac97989ab64f9262859f7bab59d23615
Jesse Sheehan <jesse@sheehan.nz>
2018-09-28 08:35:11 +1200

    Improved documentation for relay file.

--- relay.ino
+++ relay.ino
@@ -6,8 +6,15 @@ static uint8_t *relays;
 // num_relays stores the number of relays that exist
 static uint8_t num_relays;
 
-// relaySetup initialises s relays on the pins supplied and turns them off
-// returns true if successful, false if unsuccessful
+#define RELAY_OFF HIGH
+#define RELAY_ON LOW
+
+// relaySetup initialises s relays on the pins supplied and turns them off.
+// 
+// s is the length of the pins array.
+// pins is the array of pins that each relay is connected to.
+// 
+// returns true if successful, false if unsuccessful.
 bool relaySetup(uint8_t s, uint8_t pins[]) {
     relays = (uint8_t*)malloc(s * sizeof(uint8_t));
     if (relays == NULL) {
@@ -25,16 +32,16 @@ bool relaySetup(uint8_t s, uint8_t pins[]) {
     return true;
 }
 
-
-
-// relayTurnOn turns on the nth relay
-// relay numbers start at 1
+// relayTurnOn turns on the nth relay. relay numbers start at 1.
+// 
+// n is the relay number to turn on
 void relayTurnOn(uint8_t n) {
-    digitalWrite(relays[n - 1], LOW);
+    digitalWrite(relays[n - 1], RELAY_ON);
 }
 
-// relayTurnOff turns off the nth relay
-// relay numbers start at 1
+// relayTurnOff turns off the nth relay. relay numbers start at 1.
+// 
+// n is the relay number to turn off.
 void relayTurnOff(uint8_t n) {
-    digitalWrite(relays[n - 1], HIGH);
+    digitalWrite(relays[n - 1], RELAY_OFF);
 }
