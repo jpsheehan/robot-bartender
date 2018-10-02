96a5b1a16d4d91eb4f0d3e16d082aa251b7192d5
Jesse Sheehan <jps111@uclive.ac.nz>
2018-10-02 16:31:52 +1300

    Moved some of the test modes into their own file. Changed proximityGetDistance to return a float. UNTESTED!

--- bartender.ino
+++ bartender.ino
@@ -1,77 +1,28 @@
 #include <stdbool.h>
-#include <dmtimer.h>
 
 #define NUM_RELAYS 6
 
-DMTimer buttonTimer(1000000); // 1 s timer
-
-char drinks[6][10] = {
-    "Whisky",
-    "Bourbon",
-    "Rum",
-    "Coke",
-    "Gin",
-    "Vodka"
-};
-
 char prompt[] = "Select Drink";
 
-void setup() {
-    // setup the relays
-    uint8_t relayPins[NUM_RELAYS] = {11, 10, 9, 8, 7, 6};
-    relaySetup(NUM_RELAYS, relayPins);
-    lcdSetup();
-    buttonSetup(3, 2, 1);
-    proximitySetup(12, 13);
-    
+void displayWelcomeMessage() {
     lcdClear();
     lcdPrintCentered("Welcome!", 0);
     delay(500);
 }
 
+
+void setup() {
+    // setup the relays
+    uint8_t relayPins[NUM_RELAYS] = {11, 10, 9, 8, 7, 6, 5, 4};
+    relaySetup(NUM_RELAYS, relayPins);
+    lcdSetup();
+    buttonSetup(3, 2, 1);
+    proximitySetup(12, 13);
+    
+}
+
 void loop() {
 
-
-    // turn each relay on in sequence. this is a simple demo
-    // relays are accessed using one-based indexing as this is how they are labelled on the board
-//    int previousRelayIndex = NUM_RELAYS;
-//    for (uint8_t i = 1; i <= NUM_RELAYS; i++) {
-//
-//        lcdDisplayMenu(prompt, drinks[i-1], true, true);
-//        relayTurnOff(previousRelayIndex);
-//        relayTurnOn(i);
-//        previousRelayIndex = i;
-//        delay(1000);
-//    }
-
-//  // A button test:
-//  if (buttonTimer.isTimeReached()){
-//    if (buttonLeftPressed()) {
-//      lcdPrintCentered("Left", 0);
-//    }
-//    else if (buttonRightPressed()) {
-//      lcdPrintCentered("Right", 0);
-//    }
-//    else if (buttonCentrePressed()) {
-//      lcdPrintCentered("Centre", 0);
-//    }
-//    else {
-//      lcdClear();
-//    }
-//  }
-
-  if (buttonTimer.isTimeReached()) {
-    char message[16] = {0};
-    long distance = proximityGetDistance();
-
-    if (distance <= 0 || distance >= 200) {
-      snprintf(message, 16, "%s", "Out of range");
-    } else {
-      snprintf(message, 16, "%d cm", distance);
-    }
-    lcdClear();
-    lcdPrintCentered("Distance:", 0);
-    lcdPrintCentered(message, 1);
-  }
+  modeDisplayDistance();
 
 }
